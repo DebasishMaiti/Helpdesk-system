4e6f9a1e6f848a9acc36fd00360cfafc1a3ad78a
DebasishMaiti <maiti.debasish2001@gmail.com>
2025-04-25 15:13:28 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 mongoose.connect(process.env.mongodbConnection)
     .then(() => console.log('connection Established'))
     .catch((err) => console.log(err))
+server.get('/', (req, res) => {
+    res.send("Debasish")
+})
 
 server.use('/api', AllRoutes);
 
